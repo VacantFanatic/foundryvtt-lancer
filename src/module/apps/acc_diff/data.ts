a02537475b63c450fc8d23fd48e1c3a111539c29
Eranziel <eranziel@gmail.com>
2026-05-03 22:39:17 -0600

    Make attack HUD smarter about melee, thrown, and cover
    Fixes #887

--- src/module/apps/acc_diff/data.ts
+++ src/module/apps/acc_diff/data.ts
@@ -9,6 +9,7 @@ import Invisibility from "./invisibility";
 import Spotter from "./spotter";
 import { LancerToken } from "../../token";
 import { Tag } from "../../models/bits/tag";
+import { NpcFeatureType, WeaponType } from "../../enums";
 
 export enum Cover {
   None = 0,
@@ -24,6 +25,8 @@ export class AccDiffHudWeapon {
   accurate: boolean;
   inaccurate: boolean;
   seeking: boolean;
+  melee: boolean;
+  thrown: boolean;
   engaged: boolean;
   #data!: AccDiffHudData; // never use this class before calling hydrate
   plugins: { [k: string]: AccDiffHudPluginData };
@@ -35,6 +38,8 @@ export class AccDiffHudWeapon {
       accurate: t.boolean,
       inaccurate: t.boolean,
       seeking: t.boolean,
+      melee: t.boolean,
+      thrown: t.boolean,
       engaged: t.boolean,
       plugins: t.type(this.pluginSchema),
     };
@@ -51,6 +56,8 @@ export class AccDiffHudWeapon {
     this.accurate = obj.accurate;
     this.inaccurate = obj.inaccurate;
     this.seeking = obj.seeking;
+    this.melee = obj.melee;
+    this.thrown = obj.thrown;
     this.engaged = obj.engaged;
     this.plugins = obj.plugins;
   }
@@ -60,6 +67,8 @@ export class AccDiffHudWeapon {
       accurate: this.accurate,
       inaccurate: this.inaccurate,
       seeking: this.seeking,
+      melee: this.melee,
+      thrown: this.thrown,
       engaged: this.engaged,
       plugins: this.plugins,
     };
@@ -77,9 +86,9 @@ export class AccDiffHudWeapon {
     return (
       (this.accurate ? 1 : 0) -
       (this.inaccurate ? 1 : 0) -
-      (this.seeking ? 0 : cover) -
       (this.impaired ? 1 : 0) -
-      (this.engaged ? 1 : 0)
+      (this.engaged ? 1 : 0) -
+      (this.seeking || (this.melee && !this.thrown) ? 0 : cover)
     );
   }
 
@@ -386,10 +395,19 @@ export class AccDiffHudData {
     flat?: number,
     starting?: [number, number] | number
   ): AccDiffHudData {
-    let weapon = {
+    const weaponItem =
+      runtimeData instanceof LancerItem &&
+      (runtimeData.is_mech_weapon() ||
+        runtimeData.is_pilot_weapon() ||
+        (runtimeData.is_npc_feature() && runtimeData.system.type === NpcFeatureType.Weapon))
+        ? runtimeData
+        : null;
+    const weapon = {
       accurate: false,
       inaccurate: false,
       seeking: false,
+      melee: weaponItem?.currentProfile().type === WeaponType.Melee || false,
+      thrown: false,
       engaged: false,
       plugins: {} as { [k: string]: any },
     };
@@ -401,7 +419,7 @@ export class AccDiffHudData {
       starting = starting >= 0 ? [starting, 0] : [0, -starting];
     }
 
-    for (let tag of tags || []) {
+    for (const tag of tags || []) {
       switch (tag.lid) {
         case "tg_accurate":
           weapon.accurate = true;
@@ -411,6 +429,9 @@ export class AccDiffHudData {
           break;
         case "tg_seeking":
           weapon.seeking = true;
+          break;
+        case "tg_thrown":
+          weapon.thrown = true;
           break;
       }
     }
